1753de47a16fa2b4a455f1484b47ae3e5fbc5f81
Matthew-Steven-Gonzalez <gonzalez.matthew.steven@gmail.com>
2023-05-05 20:31:41 -0400

    first commit

---
+++ src/main/java/com/panaderiatias/panaderia_tias/PanaderiaTiasApplication.java
@@ -0,0 +1,13 @@
+package com.panaderiatias.panaderia_tias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaderiaTiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanaderiaTiasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panaderiatias/panaderia_tias/PanaderiaTiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panaderiatias.panaderia_tias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaderiaTiasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
